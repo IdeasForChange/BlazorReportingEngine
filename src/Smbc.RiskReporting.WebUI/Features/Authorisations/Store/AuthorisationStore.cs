b00473aff683b9ec99698fd0b75610412bf63500
Rajesh Kumar <rajesh.kumar@hotmail.co.uk>
2026-08-06 00:37:55 +0100

    Added the relevant projects to the repository

---
+++ src/Smbc.RiskReporting.WebUI/Features/Authorisations/Store/AuthorisationStore.cs
@@ -0,0 +1,94 @@
+﻿using Fluxor;
+using Smbc.RiskReporting.WebUI.Features.Themes.Store;
+
+namespace Smbc.RiskReporting.WebUI.Features.Authorisations.Store;
+
+[FeatureState]
+public record UserState
+{
+    public bool IsLoading { get; init; }
+    public string? AdAccount { get; init; }
+    public List<string> Permissions { get; init; } = [];
+    public string? ErrorMessage { get; init; }
+}
+
+// FEATURE
+public class UserStateFeature : Feature<UserState>
+{
+    public override string GetName() => nameof(UserState);
+    protected override UserState GetInitialState()
+    {
+        return new UserState
+        {
+            IsLoading = false,
+            AdAccount = null,
+            Permissions = [],
+            ErrorMessage = null
+        };
+    }
+}
+
+// ACTIONS
+public record FetchPermissionsAction(string AdAccount);
+public record FetchPermissionsSuccessAction(string AdAccount, List<string> Permissions);
+public record FetchPermissionsFailureAction(string ErrorMessage);
+
+// REDUCERS
+public static class UserReducers
+{
+    [ReducerMethod]
+    public static UserState OnFetchPermissions(UserState state, FetchPermissionsAction action) 
+    {
+        return state with
+        {
+            IsLoading = true,
+            AdAccount = action.AdAccount,
+            Permissions = state.Permissions,
+            ErrorMessage = null
+        };
+    }
+
+    [ReducerMethod]
+    public static UserState OnFetchPermissionsSuccess(UserState state, FetchPermissionsSuccessAction action)
+    {
+        return state with
+        {
+            IsLoading = false,
+            AdAccount = action.AdAccount,
+            Permissions = action.Permissions,
+            ErrorMessage = null
+        };
+    }
+
+    [ReducerMethod]
+    public static UserState OnFetchPermissionsFailure(UserState state, FetchPermissionsFailureAction action)
+    {
+        return state with
+        {
+            IsLoading = false,
+            AdAccount = state.AdAccount,
+            Permissions = [],
+            ErrorMessage = action.ErrorMessage
+        }; 
+    }
+}
+
+// EFFECTS
+//public class UserEffects(IPermissionApiService permissionService)
+//{
+//    private readonly IPermissionApiService _permissionService = permissionService;
+
+//    [EffectMethod]
+//    public async Task HandleFetchPermissionsAction(FetchPermissionsAction action, IDispatcher dispatcher)
+//    {
+//        try
+//        {
+//            var permissions = await _permissionService.GetPermissionsAsync(action.AdAccount);
+//            dispatcher.Dispatch(new FetchPermissionsSuccessAction(action.AdAccount, permissions));
+//        }
+//        catch (Exception ex)
+//        {
+//            dispatcher.Dispatch(new FetchPermissionsFailureAction(ex.Message));
+//        }
+//    }
+//}
---
+++ src/Smbc.RiskReporting.WebUI/Features/Themes/Store/ThemeStore.cs
@@ -0,0 +1,61 @@
+﻿using Fluxor;
+
+namespace Smbc.RiskReporting.WebUI.Features.Themes.Store;
+
+// STATE 
+public record ThemeState 
+{
+    public bool IsDarkMode { get; init; }
+}
+
+// FEATURE
+public class ThemeFeature : Feature<ThemeState>
+{
+    public override string GetName() => nameof(ThemeFeature);
+
+    protected override ThemeState GetInitialState()
+    {
+        return new ThemeState
+        {
+            IsDarkMode = false
+        };
+    }
+}
+
+// ACTIONS
+public record LoadDarkModeAction();
+public record ToggleDarkModeAction();
+public record SetDarkModeAction(bool IsDarkMode);
+
+// REDUCERS 
+public static class ThemeReducers 
+{
+    [ReducerMethod]
+    public static ThemeState OnSetDarkMode(ThemeState state, SetDarkModeAction action) 
+    {
+        return state with
+        {
+            IsDarkMode = action.IsDarkMode
+        };
+    }
+}
+
+//public class ThemeEffects(ApplicationSettingRepository appSettingRepo)
+//{
+//    private const string APP_SETTING_IS_DARK_MODE = "IsDarkMode";
+//    private readonly ApplicationSettingRepository _appSettingRepo = appSettingRepo;
+
+//    [EffectMethod(typeof(LoadDarkModeAction))]
+//    public async Task OnLoadDarkMode(IDispatcher dispatcher) 
+//    {
+//        var appSetting = await _appSettingRepo.GetOrAdd(APP_SETTING_IS_DARK_MODE, "false");
+//        dispatcher.Dispatch(new SetDarkModeAction(bool.Parse(appSetting.Value)));
+//    }
+
+//    [EffectMethod(typeof(ToggleDarkModeAction))]
+//    public async Task OnToggleDarkModeAction(IDispatcher dispatcher)
+//    {
+//        var appSetting = await _appSettingRepo.Toggle(APP_SETTING_IS_DARK_MODE);
+//        dispatcher.Dispatch(new SetDarkModeAction(bool.Parse(appSetting.Value)));
+//    }
+//}
